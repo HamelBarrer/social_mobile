eff6ec0846ef44e7b9d29860955a5d7395428adf
Hamel Barrero <hamel_-barrero@hotmail.com>
2023-03-14 21:15:19 -0500

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:social_mobile/screens/home_screen.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
